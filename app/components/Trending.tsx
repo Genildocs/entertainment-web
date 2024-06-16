704d43988139c73817ae9e638e2e8d632ec62799
genildocs <genildocs@gmail.com>
2024-06-16 19:55:39 -0300

    commit

--- app/components/Trending.tsx
+++ app/components/Trending.tsx
@@ -1,18 +1,18 @@
-'use client';
-import { useSearch } from '../context/SearchContext';
+"use client";
+import { useSearch } from "../context/SearchContext";
 import {
   CarouselComponent,
   CarouselItemsDirective,
   CarouselItemDirective,
-} from '@syncfusion/ej2-react-navigations';
-import { registerLicense } from '@syncfusion/ej2-base';
+} from "@syncfusion/ej2-react-navigations";
+import { registerLicense } from "@syncfusion/ej2-base";
 const key: any = process.env.NEXT_PUBLIC_EJ2_LICENSE_KEY;
 registerLicense(key);
 export default function Trending() {
   const { data } = useSearch();
   return (
     <div className="pl-3 py-5">
-      <span className="text-white text-lg ">Trending</span>
+      <span className="text-white text-2xl ">Trending</span>
       <CarouselComponent></CarouselComponent>
     </div>
   );
