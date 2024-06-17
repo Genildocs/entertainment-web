fa730bbdc46acc035409501bb4b2d638ab8c4dee
genildocs <genildocs@gmail.com>
2024-06-17 02:01:54 -0300

    commit

--- app/components/Recommended.tsx
+++ app/components/Recommended.tsx
@@ -1,14 +1,12 @@
-'use client';
-import { useSearch } from '../context/SearchContext';
-import BtnBookmarked from './BtnBookmarked';
+"use client";
+import { useSearch } from "../context/SearchContext";
+import BtnBookmarked from "./BtnBookmarked";
 export default function Recommended() {
   const { data } = useSearch();
   return (
     <div className="pl-3">
       <span className="text-white text-lg ">Recommended for you</span>
-      <div>
-        <BtnBookmarked />
-      </div>
+      <div></div>
     </div>
   );
 }
--- app/components/Trending.tsx
+++ app/components/Trending.tsx
@@ -6,14 +6,31 @@ import {
   CarouselItemDirective,
 } from "@syncfusion/ej2-react-navigations";
 import { registerLicense } from "@syncfusion/ej2-base";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 const key: any = process.env.NEXT_PUBLIC_EJ2_LICENSE_KEY;
 registerLicense(key);
 export default function Trending() {
   const { data } = useSearch();
+  const [trending, setTrending] = useState([]);
+  console.log(trending);
+  useEffect(() => {
+    const filterTrending = (items: any) => {
+      return items.filter((item: any) => item.isTrending === true);
+    };
+
+    const isTrending = filterTrending(data);
+    setTrending(isTrending);
+  }, [data]);
+
   return (
     <div className="pl-3 py-5">
-      <span className="text-white text-2xl ">Trending</span>
-      <CarouselComponent></CarouselComponent>
+      <span className="text-white text-lg ">Trending</span>
+      <div className="control-container">
+        <CarouselComponent>
+          <CarouselItemsDirective></CarouselItemsDirective>
+        </CarouselComponent>
+      </div>
     </div>
   );
 }
