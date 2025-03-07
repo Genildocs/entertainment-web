11b90fc7f491edfea8d3ce1e677425e35a1ea2c3
genildocs <genildocs@gmail.com>
2025-03-07 06:29:00 -0300

    first commit

---
+++ src/app/tv-series/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function TvSeries() {
+  return <div>TvSeries</div>;
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <h3>Desenvolvido em next-js</h3>
+    </div>
+  );
+}
