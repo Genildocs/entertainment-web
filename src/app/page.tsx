3c6b1ae74402e002edc128f7b8ff81b935c37bd7
genildocs <genildocs@gmail.com>
2025-03-09 05:47:43 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import InputSearch from '@/components/InputSearch';
 import useDataStore from '@/store/dataStore';
 import Trending from '@/components/Trending';
 export default function Home() {
-  const { filteredList } = useDataStore();
-
   return (
     <div>
       <Header />
