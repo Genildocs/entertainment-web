fc7e4fd03ce1eb813c4bcabe57d69e6d565667ca
genildocs <genildocs@gmail.com>
2025-03-23 09:19:59 -0300

    config roteamento no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import AppLayout from './layout/AppLayout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        {/* Entertainment layout */}
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Home />} />
+          </Route>
+          {/* fallback route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
